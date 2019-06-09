23fd63d30e30f8e0c5ab6dac47229571e4060f20
unknown <batemanvo@gmail.com>
2019-06-09 12:05:40 -0700

    Renamed EventRegionCommonEvent to fix a typo in the name, and fixed bugs with Common Event trying to reference a region ID Map used for regionConditions.

--- js/plugins/EventRegionCommonEvent.js
+++ js/plugins/EventRegionCommonEvent.js
@@ -8,9 +8,18 @@
 * number of the common event to call if the event moves onto the specified region
 * ID.
 *
+* IMPORTANT: By default, this functionality is tied to Switch 15, so that it only
+* works if that switch is ON. Ensure that when you want the common event
+* functionality to work, you have turned the corresponding Switch ON. You can
+* change the switch number in the Plugin parameters.
+*
 * Alternatively, if you put a comment at the top of a page's list of commands
 * with the format <regionCondition: x>, where x is the region ID, then that
 * page will execute whenever the event moves onto a tile with that ID.
+*
+* You can optionally add comma-separated regions to indicate that the page
+* should run for any of those regions, such as <regionCondition: 1, 2, 3>
+* indicating that the page condition applies to any of those regions.
 *
 * Note that a comment with the text "<regionCondition: x>" must be the first
 * command at the top of a page. When using regionCondition, all other
@@ -22,6 +31,15 @@
 * page commands will run, but tiles 2, 3, 4, and 5 will not execute any
 * logic until the event steps off a tile with that region ID and steps
 * back on another tile with that region.
+*
+* If using comma-separated regions for a single page, each unique
+* region ID is considered a separate "area" for the above functionality.
+*
+* @param switchNumber
+* @text Enable Region Common Events
+* @desc If this switch number is ON, then the common events for region IDs in the note box of an event will run.
+* @type switch
+* @default 15
 */
 
 (function(module) {
@@ -30,6 +48,14 @@
     module.Zevia = module.Zevia || {};
     var EventRegionCommonEvent = module.Zevia.EventRegionCommonEvent = {};
     var COMMENT_CODE = 108;
+
+    var regionCommonEventSwitchNumber = parseInt(PluginManager.parameters('EventRegionCommonEvent').switchNumber);
+    if (isNaN(regionCommonEventSwitchNumber)) {
+        throw new Error(
+            'Configuration Error: EventRegionCommonEvent requires a valid switch number to be' +
+            ' specified in the setup parameters'
+        );
+    }
 
     EventRegionCommonEvent.initialize = Game_Event.prototype.initialize;
     Game_Event.prototype.initialize = function(mapId, eventId) {
@@ -42,6 +68,8 @@
     };
 
     Game_Event.prototype.regionCommonEvent = function() {
+        if (!$gameSwitches.value(regionCommonEventSwitchNumber)) { return; }
+
         var regionCommonEvent = $dataMap.events[this._eventId].meta.regionCommonEvent;
         if (!regionCommonEvent) { return; }
 
@@ -50,12 +78,12 @@
 
         var regionId = this.regionId();
         if (regionId === parseInt(regionAndEventIds[0])) {
-            if (!this.hasRunRegionEvent(regionId)) {
+            if (!this._hasRunCommonEvent) {
                 $gameTemp.reserveCommonEvent(parseInt(regionAndEventIds[1]));
-                this._executedRegionEvents[regionId] = true;
+                this._hasRunCommonEvent = true;
             }
         } else {
-            this._executedRegionEvents[regionId] = false;
+            this._hasRunCommonEvent = false;
         }
     };
 
@@ -73,6 +101,7 @@
     Game_Event.prototype.regionConditions = function() {
         var pages = this.event().pages;
         var hasRegionPage = false;
+        var regionId = this.regionId();
         for (var i = pages.length - 1; i >= 0; i--) {
             var currentPage = pages[i];
             if (!EventRegionCommonEvent.meetsConditions.call(this, currentPage)) { continue; }
@@ -80,20 +109,25 @@
             var firstItem = currentPage.list[0];
             if (firstItem.code !== COMMENT_CODE) { continue; }
 
-            var regionCondition = firstItem.parameters[0].match(/<regionCondition:(\s?|\s+?)\d+(\s+?|\s?)>/);
+            var regionCondition = firstItem.parameters[0].match(/<regionCondition:(.*)>/);
             if (!regionCondition) { continue; }
 
-            var regionId = regionCondition[0].match(/\d+/);
-            if (!regionId) { continue; }
+            var regionIds = regionCondition[1];
+            if (!regionIds) { continue; }
 
-            if (this.regionId() === parseInt(regionId[0])) {
-                hasRegionPage = true;
-                this._pageIndex = i;
-                this.setupPage();
+            var allIds = regionIds.match(/\d+/g);
+            if (!allIds) { continue; }
+
+            for (var j = 0; j < allIds.length; j++) {
+                if (regionId === parseInt(allIds[j])) {
+                    hasRegionPage = true;
+                    this._pageIndex = i;
+                    this.setupPage();
+                    break;
+                }
             }
         }
 
-        var regionId = this.regionId();
         if (!hasRegionPage) {
             this._executedRegionEvents[regionId] = false;
         } else if (!this.hasRunRegionEvent(regionId)) {
